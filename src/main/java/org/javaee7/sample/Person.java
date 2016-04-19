f767a926771591c38ec13198bd92354b8aeca9af
Arthur Bordessoules <arthur.bordessoules@gmail.com>
2016-04-19 15:11:16 +0200

    first commit

--- src/main/java/org/javaee7/sample/Person.java
+++ src/main/java/org/javaee7/sample/Person.java
@@ -8,18 +8,23 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Person {
     private String name;
-
+	private String mail ;
+	
     public Person() {
     }
 
-    public Person(String name) {
+    public Person(String name, String mail) {
         this.name = name;
+		this.mail = mail;
     }
     
     public String getName() {
         return name;
     }
 
+	public String getMail() {
+        return mail;
+    }
     public void setName(String name) {
         this.name = name;
     }
